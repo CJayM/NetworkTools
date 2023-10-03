7c9e0f96c51a58005457394db6abc8075540c035
cjaym_notebook <cjaym@yandex.ru>
2023-10-03 21:16:45 +0300

    Сделано многопоточно

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,7 +2,74 @@
 #include "ui_mainwindow.h"
 
 #include <QDebug>
+#include <QFuture>
+#include <QFutureWatcher>
 #include <QProcess>
+#include <QtConcurrent>
+#include <utility>
+
+QList<std::pair<QString, int>> collectAddresses(QString pref, int from,
+                                                int to) {
+  QList<std::pair<QString, int>> result;
+
+  int pos = from;
+  int indx = 0;
+  while (pos < to) {
+    result.append(std::make_pair(QString("%1%2").arg(pref).arg(pos), indx));
+    ++pos;
+    ++indx;
+  }
+
+  return result;
+}
+
+std::pair<QString, int> parsePingOutput(QString data, QString addr) {
+  if (data.isNull())
+    return std::make_pair<QString, int>("", -1);
+
+  QString percent;
+  int percentValue = -1;
+
+  auto lines = data.split("\r\n");
+  auto end = lines.size();
+  int pos = 0;
+
+  while (pos < end) {
+    auto line = lines[pos];
+    if (line.isEmpty() == false) {
+      if (line.startsWith("Ping statistics for ")) {
+        ++pos;
+        auto text = lines[pos];
+        percent = text.split(" ")[14].mid(1);
+        percentValue = percent.mid(0, percent.size() - 1).toInt();
+        break;
+      }
+    }
+
+    ++pos;
+  }
+
+  return std::make_pair(addr, percentValue);
+}
+
+std::tuple<QString, int, int> ping(std::pair<QString, int> pair) {
+  auto addr = pair.first;
+  auto pos = pair.second;
+
+  QString program = "ping";
+  QStringList arguments;
+  arguments << "-n"
+            << "2"
+            << "-w"
+            << "10" << addr;
+
+  QProcess process;
+  process.start(program, arguments);
+  bool isOk = process.waitForFinished(10000);
+  auto data = process.readAllStandardOutput();
+  auto res = parsePingOutput(data, addr);
+  return std::make_tuple(res.first, res.second, pos);
+}
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
@@ -17,109 +84,66 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow() { delete ui; }
 
 void MainWindow::onScanClicked() {
-  if (gitProcess == nullptr) {
-    ui->progressBar->show();
-    ui->pushButton->setText("Stop");
-    startPing(ui->spinBox->value());
-  } else {
+  if (isScanned_) {
+    isScanned_ = false;
     ui->pushButton->setText("Scan");
     ui->progressBar->hide();
-    gitProcess->kill();
-    gitProcess->deleteLater();
-    gitProcess = nullptr;
-  }
-}
-
-void MainWindow::readSuccess(QString normal) {
-  if (normal.isNull())
-    return;
-
-  auto lines = normal.split("\r\n");
-  auto end = lines.size();
-  int pos = 0;
-  while (pos < end) {
-    auto line = lines[pos];
-    if (line.isEmpty() == false) {
-
-      if (line.startsWith("Ping statistics for ")) {
-        ++pos;
-        auto text = lines[pos];
-        auto percents = text.split(" ")[14].mid(1);
-        if (percents != "100%") {
-          qDebug() << percents;
-          auto row = ui->tableWidget->rowCount();
-          ui->tableWidget->insertRow(row);
-          ui->tableWidget->setItem(
-              row, 0, new QTableWidgetItem(QString("192.168.1.%1").arg(addr_)));
-          ui->tableWidget->setItem(row, 1, new QTableWidgetItem(percents));
-        }
-      }
-    }
-
-    auto len = ui->spinBox_2->value() - ui->spinBox->value();
-    auto percent = 100.0 / len * (addr_ - ui->spinBox->value());
-    ui->progressBar->setValue(percent);
-
-    ++pos;
-  }
-
-  qDebug() << normal;
-}
-
-void MainWindow::onFinish(int exitCode, QProcess::ExitStatus exitStatus) {
-  auto normal = gitProcess->readAllStandardOutput();
-  auto error = gitProcess->readAllStandardError();
-  if (error.isNull() == false) {
-    qDebug() << "ERROR: " << error;
-    //	ui->statusBar()->showMessage(QString("ERROR: %1").arg(QString(error)));
-  }
-  if (normal.isNull() == false) {
-    readSuccess(normal);
-    if (addr_ < ui->spinBox_2->value())
-      startPing(addr_ + 1);
-    else
-      qDebug() << "FINISH";
   } else {
-    gitProcess->deleteLater();
-    gitProcess = nullptr;
+    isScanned_ = true;
+    ui->progressBar->show();
+    ui->pushButton->setText("Stop");
+    addresses_ = collectAddresses(ui->lineEdit->text(), ui->spinBox->value(),
+                                  ui->spinBox_2->value());
+
+    future_ = QtConcurrent::mapped(addresses_, ping);
+
+    auto watcher = new QFutureWatcher<std::tuple<QString, int, int>>(this);
+    connect(watcher, &QFutureWatcher<QString>::progressValueChanged, this,
+            &MainWindow::onFutureProgress);
+    connect(watcher, &QFutureWatcher<QString>::canceled, this,
+            []() { qDebug() << "Canceled"; });
+    connect(watcher, &QFutureWatcher<QString>::paused, this,
+            []() { qDebug() << "Paused"; });
+    connect(watcher, &QFutureWatcher<QString>::destroyed, this,
+            []() { qDebug() << "Destroyed"; });
+    connect(watcher, &QFutureWatcher<QString>::finished, this,
+            &MainWindow::onFinished);
+
+    connect(watcher, &QFutureWatcher<QString>::resultReadyAt, this,
+            &MainWindow::onResultReadyAt);
+
+    connect(watcher, &QFutureWatcher<QString>::resumed, this,
+            []() { qDebug() << "Resumed"; });
+    connect(watcher, &QFutureWatcher<QString>::started, this,
+            &MainWindow::onStarted);
+
+    watcher->setFuture(future_);
   }
 }
 
-void MainWindow::onError(QProcess::ProcessError error) {
-  qDebug() << "ERROR" << error;
-  if (gitProcess != nullptr)
-    qDebug() << gitProcess->readAllStandardOutput();
-  //  gitProcess->deleteLater();
-  //  gitProcess = nullptr;
+void MainWindow::onFutureProgress(int progressValue) {
+  ui->progressBar->setValue(100.0 / addresses_.size() * progressValue);
 }
 
-void MainWindow::startPing(int addr) {
-  addr_ = addr;
+void MainWindow::onResultReadyAt(int index) {
+  auto result = future_.resultAt(index);
   ui->statusbar->showMessage(
-      QString("Looking for %1").arg(QString("192.168.1.%1").arg(addr)));
+      QString("Processing output from %1...").arg(std::get<0>(result)));
+  auto row = ui->tableWidget->rowCount();
+  ui->tableWidget->insertRow(row);
+  ui->tableWidget->setItem(row, 0, new QTableWidgetItem(std::get<0>(result)));
+  ui->tableWidget->setItem(
+      row, 1, new QTableWidgetItem(QString::number(std::get<1>(result))));
+}
 
-  QString program = "ping";
-  QStringList arguments;
-  arguments << "-n"
-            << "2"
-            << "-w"
-            << "10" << QString("192.168.1.%1").arg(addr);
+void MainWindow::onStarted() {
+  ui->progressBar->show();
+  ui->pushButton->setText("Stop");
+  ui->statusbar->showMessage(QString("Starting..."));
+}
 
-  if (gitProcess != nullptr)
-    gitProcess->deleteLater();
-
-  gitProcess = new QProcess(this);
-
-  connect(
-      gitProcess,
-      static_cast<void (QProcess::*)(
-          int exitCode, QProcess::ExitStatus exitStatus)>(&QProcess::finished),
-      this, &MainWindow::onFinish);
-  //  connect(gitProcess, &QProcess::readyRead, this, &MainWindow::onReadyRead);
-  connect(
-      gitProcess,
-      static_cast<void (QProcess::*)(QProcess::ProcessError)>(&QProcess::error),
-      this, &MainWindow::onError);
-
-  gitProcess->start(program, arguments);
+void MainWindow::onFinished() {
+  ui->pushButton->setText("Scan");
+  ui->progressBar->hide();
+  ui->statusbar->showMessage(QString("Finish"));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,8 @@
 #pragma once
 
+#include <QFuture>
 #include <QMainWindow>
-#include <QProcess>
+//#include <QProcess>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,13 +20,19 @@ public:
 private slots:
   void onScanClicked();
   //  void onReadyRead();
-  void onFinish(int exitCode, QProcess::ExitStatus exitStatus);
-  void onError(QProcess::ProcessError error);
+  //  void onFinish(int exitCode, QProcess::ExitStatus exitStatus);
+  //  void onError(QProcess::ProcessError error);
+  void onFutureProgress(int progressValue);
+  void onResultReadyAt(int index);
+  void onStarted();
+  void onFinished();
 
 private:
   Ui::MainWindow *ui;
-  QProcess *gitProcess = nullptr;
   int addr_ = 1;
   void startPing(int addr);
-  void readSuccess(QString data);
+  //  void readSuccess(QString data);
+  bool isScanned_ = false;
+  QFuture<std::tuple<QString, int, int>> future_;
+  QList<std::pair<QString, int>> addresses_;
 };
